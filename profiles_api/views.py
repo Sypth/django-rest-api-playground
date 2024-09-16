8d58bf788650019aef753edbc8cdb275ba84af0b
Sypth <105406568+Sypth@users.noreply.github.com>
2024-09-16 09:11:01 +0800

    Added POST, PUT, PATCH, and DELETE methods to our APIView

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,16 +1,43 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-class HelloAPIView(APIView):
-    """Test API View"""
+from profiles_api import serializers
+
+class TestApi(APIView):
+    """afdhasdfhasdfh"""
+
+    serializer_class = serializers.TestSerializers
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
         an_apiview = [
-            'Uses HTTP methods as function (get, post, patch, put, delete)',
-            'Is similar to a traditional Django View',
-            'Gives you the most control over your application logic',
-            'Is mapped manually to URLs',
+            "Kupal si Paz"
         ]
 
-        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+        return Response({'message': 'Kupal kaba?!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message my name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Wassup {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=400
+            )
+
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method': "PUT"}) 
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method': 'DELETE'})
